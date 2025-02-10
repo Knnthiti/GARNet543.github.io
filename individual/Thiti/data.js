dd5982e6338291acda8ce95c215d034df404a7cd
Knnthiti <120441472+Knnthiti@users.noreply.github.com>
2025-02-11 02:07:59 +0700

    Update data.js

--- individual/Thiti/data.js
+++ individual/Thiti/data.js
@@ -2,14 +2,30 @@ export const testimonials = [
   ];
 
 export const skills =[
+ {
+    id:1,
+    icon:"../img/oop.png",
+    alt:"OOP",
+    title:"OOP",
+    level:"Intermediate",
+    description:"build library for hardware"
+ },
+ {
+    id:2,
+    icon:"../img/oop.png",
+    alt:"STM32",
+    title:"STM32",
+    level:"Beginner to Intermediate",
+    description:"make Frameware for robot"
+  },
   {
-  id:1,
-  icon:"",
-  alt: "",
-  title:"",
-  level:"",
-  description:""
-},
+    id:3,
+    icon:"../img/oop.png",
+    alt:"EasyEDA",
+    title:"EasyEDA",
+    level:"Beginner to Intermediate",
+    description:"make PCB for microcontroller"
+  },
 ];
 
 export const features = [];
